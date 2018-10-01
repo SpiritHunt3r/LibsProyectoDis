eeebf323cf231e7ee22b20da205e9d403d1d5813
Juan Jose Araya Castro <juan_jop96@hotmail.com>
2018-10-01 01:29:33 -0600

    Print en TXT
    Listo para agregarlo al archivo.

--- src/Modelo/EscritorTxT.java
+++ src/Modelo/EscritorTxT.java
@@ -4,6 +4,8 @@ package Modelo;
 
 import java.util.*;
 import Controlador.DTOAlgoritmos;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * 
@@ -26,7 +28,15 @@ public class EscritorTxT implements IEscritor {
      * @return
      */
     public boolean escribir(DTOAlgoritmos dto) {
-        
+        String print = "";
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+        LocalDateTime now = LocalDateTime.now();  
+        print += "Fecha y Hora: "+dtf.format(now)+"\n";
+        print += "Frase de Origen: "+ dto.getFraseOrigen()+ "\n******************************\n";
+        for (int i=0;i<dto.getResultados().size();i++){
+            print += dto.getResultados().get(i) + "\n******************************\n";
+        }
+        System.out.println(print);
         return true;
     }
 
